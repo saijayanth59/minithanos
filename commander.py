0d30db6b92747a2fe4c50f5d3cfe556d5cbdccf6
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-03-09 23:16:47 +0530

    fix: commander_model import error in commander.py

--- commander.py
+++ commander.py
@@ -1,10 +1,10 @@
-from models import commander
+from models import commander_model
 import subprocess
 
 while True:
     prompt = input(">>> ")
     for _ in range(3):
-        command = commander.generate(prompt)
+        command = commander_model.generate(prompt)
         print(command)
         output = subprocess.run([r"C:\Program Files\Git\bin\bash.exe", "-c", command], shell=True, capture_output=True)
         print(output)
@@ -12,5 +12,4 @@ while True:
         if output.stderr != b'':
             print(output.stderr.decode())
             continue
-        
         break 
