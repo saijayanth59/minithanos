01986312b4fe9eecd0566114f84c57db36908c7f
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-03-14 10:53:08 +0530

    feat: add function to speak using pyttsx3

--- main.py
+++ main.py
@@ -2,29 +2,38 @@ import os
 from dotenv import load_dotenv
 from RealtimeSTT import AudioToTextRecorder
 import elevenlabs
+import pyttsx3
 from agents.god import do
 
 load_dotenv()
 
 
 def process_input(text):
-    if "gemini" in text.lower():
+    if "jarvis" in text.lower():
         print(text)
         response = do(text)
         print(response)
-        audio = client.generate(
-            voice="Jessica",
-            voice_settings=elevenlabs.VoiceSettings(
-                use_speaker_boost=True,
-                speed=1,
-                stability=0.5,
-                similarity_boost=0.7,
-                style=0.3,
-            ),
-            text=response,
-            model="eleven_flash_v2_5"
-        )
-        elevenlabs.play(audio)
+        speak_pyttsx3(response)
+
+
+def speak_elevenlabs(text):
+    audio = client.generate(
+        voice="Nicole",
+        voice_settings=elevenlabs.VoiceSettings(
+            use_speaker_boost=True,
+            speed=1,
+            stability=0.5,
+            similarity_boost=0.7,
+            style=0.3,
+        ),
+        text=text,
+        model="eleven_flash_v2_5"
+    )
+    elevenlabs.play(audio)
+    
+
+def speak_pyttsx3(text):
+    pyttsx3.speak(text)
 
 
 if __name__ == "__main__":
